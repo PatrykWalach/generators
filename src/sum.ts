647a948e11ac68028dbc72b8a2f570ad06b0b847
PatrykWalach <patryk_walach_github@outlook.com>
2021-03-14 17:30:24 +0100

    feat: add overloads

--- src/sum.ts
+++ src/sum.ts
@@ -1,12 +1,21 @@
-export function sum(iterable: Iterable<number>, start?: number): number
-export function sum(iterable: Iterable<bigint>, start?: bigint): bigint
 export function sum(
-  iterable: Iterable<number> | Iterable<bigint>,
-  start: number | bigint = 0,
-): number | bigint {
+  iterable: Iterable<number | boolean>,
+  start?: number,
+): number
+export function sum(iterable: Iterable<bigint>, start: bigint): bigint
+export function sum(iterable: Iterable<any>, start: string): string
+export function sum(iterable: Iterable<string>, start?: number | bigint): string
+export function sum(
+  iterable:
+    | Iterable<number | boolean>
+    | Iterable<bigint>
+    | Iterable<any>
+    | Iterable<string>,
+  start: number | bigint | string = 0,
+): number | string | bigint {
   let acc = start
   for (const v of iterable) {
-    acc += v as any
+    acc += v
   }
   return acc
 }
--- tests/unit/sum.spec.ts
+++ tests/unit/sum.spec.ts
@@ -2,10 +2,37 @@ import { sum } from '../../src/sum'
 
 describe('sum', () => {
   it('array', () => {
-    expect(sum([2.5, 3, 4, -5])).toStrictEqual(4.5)
+    const summed: number = sum([2.5, 3, 4, -5])
+    expect(summed).toStrictEqual(4.5)
   })
 
   it('array with start', () => {
-    expect(sum([2.5, 3, 4, -5], 10)).toStrictEqual(14.5)
+    const summed: number = sum([2.5, 3, 4, -5], 10)
+    expect(summed).toStrictEqual(14.5)
+  })
+
+  it('boolean + number', () => {
+    const summed: number = sum([true, true, false, true], 1)
+    expect(summed).toStrictEqual(4)
+  })
+
+  it('number + string', () => {
+    const summed: string = sum(['a', '', 'c'], 10)
+    expect(summed).toStrictEqual('10ac')
+  })
+
+  it('bigint + string', () => {
+    const summed: string = sum(['a', '', 'c'], 10n)
+    expect(summed).toStrictEqual('10ac')
+  })
+
+  it('string + boolean', () => {
+    const summed: string = sum([true, false], 'a')
+    expect(summed).toStrictEqual('atruefalse')
+  })
+
+  it('string + any', () => {
+    const summed: string = sum([null, undefined, 12, 'b'], 'a')
+    expect(summed).toStrictEqual('anullundefined12b')
   })
 })
