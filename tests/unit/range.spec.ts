c9b1c091bf9b9636db30a25d70ce3e022853dbd4
PatrykWalach <patryk_walach_github@outlook.com>
2021-04-10 16:27:28 +0200

    feat(range): improve coverage

--- tests/unit/range.spec.ts
+++ tests/unit/range.spec.ts
@@ -1,4 +1,4 @@
-import { range, ValueError } from '../../src'
+import { range, Range, reversed, ValueError } from '../../src'
 
 describe('range', () => {
   it('infinite', () => {
@@ -21,14 +21,13 @@ describe('range', () => {
   })
 
   it('three args', () => {
-    const first = 2
-    const length = 6
-    const step = 3
-    expect([...range(first, length, step)]).toMatchObject(
-      Array.from(
-        { length: Math.ceil((length - first) / step) },
-        (_, i) => first + i * step,
-      ),
-    )
+    expect([...range(2, 6, 3)]).toMatchObject([2, 5])
+  })
+
+  it('decreasing', () => {
+    expect([...range(2, -5, -3)]).toMatchObject([2, -1, -4])
+  })
+  it('is reversible', () => {
+    expect([...reversed(range(2, 15, 3))]).toMatchObject([14, 11, 8, 5, 2])
   })
 })
