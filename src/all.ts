8e8efe04abe63bee306c9be05a66d8d92e1f065c
PatrykWalach <patryk_walach_github@outlook.com>
2021-04-23 19:30:27 +0200

    refactor(any): use all

--- src/all.ts
+++ src/all.ts
@@ -1,20 +1,17 @@
-export function all(it: Iterable<any>): boolean {
+import { map } from './map'
+
+export function all(it: Iterable<unknown>): boolean {
   for (const v of it) {
     if (v) {
       continue
     }
+
     return false
   }
 
   return true
 }
 
-export function any(it: Iterable<any>) {
-  for (const v of it) {
-    if (!v) {
-      continue
-    }
-    return true
-  }
-  return false
+export function any(it: Iterable<unknown>): boolean {
+  return !all(map(it, (v) => !v))
 }
