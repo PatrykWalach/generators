d4cf73e3aac95441f7bbffdf1ccc51d106d4aa26
PatrykWalach <patryk_walach_github@outlook.com>
2021-05-03 18:24:47 +0200

    refactor(slice): decrease complexity

--- src/slice.ts
+++ src/slice.ts
@@ -28,22 +28,20 @@ export class Slice {
     if (len < 0) {
       throw new ValueError('length should not be negative')
     }
-    let { start, step, stop } = this
+
+    let { start, stop } = this
+    const { step } = this
 
     if (start < 0) {
-      start += len
-      if (start < 0) {
-        start = step < 0 ? -1 : 0
-      }
+      const normalizedStart = step < 0 ? -1 : 0
+      start = Math.max(start + len, normalizedStart)
     } else if (start >= len) {
       start = step < 0 ? len - 1 : len
     }
 
     if (stop < 0) {
-      stop += len
-      if (stop < 0) {
-        stop = step < 0 ? -1 : 0
-      }
+      const normalizedStop = step < 0 ? -1 : 0
+      stop = Math.max(stop + len, normalizedStop)
     } else if (stop >= len) {
       stop = step < 0 ? len - 1 : len
     }
