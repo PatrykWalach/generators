64545a0335e546995b29930b8c77cc38d3c11e1e
PatrykWalach <patryk_walach_github@outlook.com>
2021-06-04 18:21:34 +0200

    refactor: use pipe

--- src/reversed.ts
+++ src/reversed.ts
@@ -1,22 +1,22 @@
 import { Reversible, __reversed__ } from './abs/Reversible'
 import { Sized } from './abs/Sized'
 import { len } from './len'
+import { pipe } from './pipe'
 import { range } from './range'
-
 
 const isReversible = <T>(
   seq: Reversible<T> | (Sized & { [index: number]: T }),
 ): seq is Reversible<T> => (seq as Reversible<T>)[__reversed__] !== undefined
 
 export function* reversed<T>(
-  seq: Reversible<T> | (Sized & { [index: number]: T }),
+  it: Reversible<T> | (Sized & { [index: number]: T }),
 ): Iterable<T> {
-  if (isReversible(seq)) {
-    yield* seq[__reversed__]()
+  if (isReversible(it)) {
+    yield* it[__reversed__]()
     return
   }
-  
-  for (const i of reversed(range(len(seq)))) {
-    yield seq[i]
+
+  for (const i of pipe(it, len, range, reversed)) {
+    yield it[i]
   }
 }
