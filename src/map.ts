47111b39bcc60025beee1c535a8a2781d6d284b7
PatrykWalach <patryk_walach_github@outlook.com>
2021-07-06 19:41:10 +0200

    feat: curry map

--- src/map.ts
+++ src/map.ts
@@ -1,32 +1,60 @@
+import { curry } from './curry'
 import { iter, next } from './iter'
-import { ignore, StopIteration } from './util'
+import { ignore, StopIteration, ValueError } from './util'
 
-export type M<T> = T extends Iterable<infer K> ? K : unknown
+export type IterableValue<T> = T extends Iterable<infer K> ? K : unknown
 
-export type F<T extends unknown[], R> = (
-  ...vs: {
-    [K in keyof T]: M<T[K]>
+export type IterableValues<T> = {
+  [K in keyof T]: IterableValue<T[K]>
+}
+
+export type F<T extends unknown[], R> = (...vs: IterableValues<T>) => R
+
+const isIterable = (o: unknown): o is Iterable<unknown> =>
+  typeof (o as Iterable<unknown>)[Symbol.iterator] === 'function'
+
+function* _map(
+  _fn: Iterable<unknown>,
+  _it: Iterable<unknown> | ((...args: unknown[]) => unknown),
+  ..._iterables: (Iterable<unknown> | ((...args: unknown[]) => unknown))[]
+) {
+  _iterables.unshift(_fn, _it)
+
+  _iterables.unshift(..._iterables.splice(_iterables.length - 1, 1))
+
+  const [fn, ...iterables] = _iterables as unknown as [
+    (...args: unknown[]) => unknown,
+    ...Iterable<unknown>[]
+  ]
+
+  if (typeof fn !== 'function') {
+    throw new ValueError(`fn is not a function`)
   }
-) => R
 
-export function* map<T extends Iterable<unknown>[], R>(
-  ...iterables: [...T, F<T, R>]
-): Generator<R> {
-  const fn = iterables.pop() as F<T, R>
+  for (const it of iterables) {
+    if (!isIterable(it)) {
+      throw new ValueError(`${it} is not iterable`)
+    }
+  }
 
-  const its = ((iterables as unknown) as T).map((iterable) => iter(iterable))
+  const its = iterables.map((iterable) => iter(iterable))
 
   try {
-    let values = its.map(next)
-    while (true) {
-      yield fn(
-        ...(values as {
-          [K in keyof T]: M<T[K]>
-        }),
-      )
-      values = its.map(next)
-    }
+    do {
+      const values = its.map(next)
+      yield fn(...values)
+    } while (true)
   } catch (e) {
     ignore(e, StopIteration)
   }
 }
+
+interface Map {
+  <T extends Iterable<unknown>[], R>(_fn: F<T, R>): (...its: T) => Iterable<R>
+
+  <T extends Iterable<unknown>[], R>(
+    ..._iterables: [...T, F<T, R>]
+  ): Iterable<R>
+}
+
+export const map = curry(_map) as unknown as Map
