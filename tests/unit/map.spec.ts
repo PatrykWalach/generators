f7ba53573a91ca666b3d5cff4454c2c03de4091a
PatrykWalach <patryk_walach_github@outlook.com>
2021-07-16 14:16:33 +0200

    feat: pipe map

--- tests/unit/map.spec.ts
+++ tests/unit/map.spec.ts
@@ -1,24 +1,34 @@
-import { map } from '../../src'
+import { map, pipe, toArray } from '../../src'
 
 describe('map()', () => {
   it('array', () => {
-    expect([...map([1, 2, 3, 4], (x) => x * x)]).toMatchObject([1, 4, 9, 16])
+    expect(
+      pipe(
+        [1, 2, 3, 4],
+        map((x) => x * x),
+        toArray,
+      ),
+    ).toMatchObject([1, 4, 9, 16])
   })
 
   it('multiple arrays', () => {
-    expect([...map([4, 5, 6], [5, 6, 7], (a, b) => a + b)]).toMatchObject([
-      9,
-      11,
-      13,
-    ])
+    expect(
+      pipe(
+        map([4, 5, 6], [5, 6, 7], (a, b) => a + b),
+        toArray,
+      ),
+    ).toMatchObject([9, 11, 13])
   })
 
   it('types', () => {
-    const it: Iterable<string> = map(
-      ['a'],
-      [1],
-      [false],
-      (str: string, n: number, nul: boolean) => str + n.toString() + nul,
+    const it: Iterable<string> = pipe(
+      map(
+        ['a'],
+        [1],
+        [false],
+        (str: string, n: number, nul: boolean) => str + n.toString() + nul,
+      ),
+      toArray,
     )
 
     expect([...it]).toMatchObject(['a1false'])
