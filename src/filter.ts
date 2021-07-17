6ce233ed6a396f9762829b7a741f4242c9f03815
PatrykWalach <patryk_walach_github@outlook.com>
2021-07-17 15:21:20 +0200

    feat(filter): curry

--- src/filter.ts
+++ src/filter.ts
@@ -1,20 +1,26 @@
+import { curry } from './curry'
 
-export function filter<T, R extends T>(
-  it: Iterable<T>,
-  condition: (v: T) => v is R,
-): Generator<R, void>
-export function filter<T>(
-  it: Iterable<T>,
-  condition: (v: T) => boolean,
-): Generator<T, void>
-export function* filter<T, R extends T>(
+export const _filter = function* <T, R extends T>(
   it: Iterable<T>,
   condition: ((v: T) => v is R) | ((v: T) => boolean),
-) {
+): Iterable<T> | Iterable<R> {
   for (const v of it) {
     if (!condition(v)) {
       continue
     }
+
     yield v
   }
 }
+
+interface Filter {
+  <T, R extends T>(condition: (v: T) => v is R): (
+    it: Iterable<T>,
+  ) => Iterable<R>
+  <T>(condition: (v: T) => boolean): (it: Iterable<T>) => Iterable<T>
+
+  <T, R extends T>(it: Iterable<T>, condition: (v: T) => v is R): Iterable<R>
+  <T>(it: Iterable<T>, condition: (v: T) => boolean): Iterable<T>
+}
+
+export const filter = curry(_filter) as Filter
