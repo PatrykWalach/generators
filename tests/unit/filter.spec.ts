df3f85613940e2f5551f597f99541f18f9ca4433
PatrykWalach <patryk_walach_github@outlook.com>
2021-07-21 18:38:06 +0200

    feat: test filter pipe

--- tests/unit/filter.spec.ts
+++ tests/unit/filter.spec.ts
@@ -1,10 +1,43 @@
-import { filter } from '../../src'
+import { filter, pipe, toArray } from '../../src'
 
 describe('filter', () => {
   it('filters', () => {
+    expect(
+      pipe(
+        [0, '1', null, undefined, '4'],
+        filter((a) => typeof a === 'string'),
+        toArray,
+      ),
+    ).toMatchObject<(string | number)[]>(['1', '4'])
+  })
+
+  it('type predicate work', () => {
+    const filtered: string[] = pipe(
+      [0, '1', null, undefined, '4'],
+      filter((a): a is string => typeof a === 'string'),
+      toArray,
+    )
+
+    expect(filtered).toMatchObject(['1', '4'])
+  })
+
+  it('type predicate expect error', () => {
+    //@ts-expect-error should be string[]
+    const filtered: null[] = pipe(
+      [0, '1', null, undefined, '4'],
+      filter((a): a is string => typeof a === 'string'),
+      toArray,
+    )
+
+    expect(filtered).toMatchObject(['1', '4'])
+  })
+})
+
+describe('data-first filter', () => {
+  it('filters', () => {
     expect([
       ...filter([0, '1', null, undefined, '4'], (a) => typeof a === 'string'),
-    ]).toMatchObject(['1', '4'])
+    ]).toMatchObject<(string | number)[]>(['1', '4'])
   })
 
   it('type predicate work', () => {
@@ -14,17 +47,19 @@ describe('filter', () => {
         (a): a is string => typeof a === 'string',
       ),
     ]
+
     expect(filtered).toMatchObject(['1', '4'])
   })
 
   it('type predicate expect error', () => {
-    //@ts-expect-error
+    //@ts-expect-error should be string[]
     const filtered: null[] = [
       ...filter(
         [0, '1', null, undefined, '4'],
         (a): a is string => typeof a === 'string',
       ),
     ]
+
     expect(filtered).toMatchObject(['1', '4'])
   })
 })
