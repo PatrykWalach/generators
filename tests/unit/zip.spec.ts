601788b1d28732ae8179c4ad32aee4026d4b1a1d
PatrykWalach <patryk_walach_github@outlook.com>
2021-07-27 19:50:45 +0200

    feat: rewrite tests

--- tests/unit/zip.spec.ts
+++ tests/unit/zip.spec.ts
@@ -1,22 +1,34 @@
-import { zip } from '../../src'
+import { iter, pipe, range, toArray, zip } from '../../src'
 
 describe('zip', () => {
   it('right longer', () => {
-    expect([...zip(['a', 'b'], ['c', 'd', 'f'])]).toMatchObject([
+    expect(pipe(zip('ab', 'cdf'), toArray)).toMatchObject([
       ['a', 'c'],
       ['b', 'd'],
     ])
   })
 
   it('left longer', () => {
-    expect([...zip(['a', 'b', 'c'], ['d', 'f'])]).toMatchObject([
+    expect(pipe(zip('abc', 'df'), toArray)).toMatchObject([
       ['a', 'd'],
       ['b', 'f'],
     ])
   })
-  it('types', () => {
-    const it: Iterable<[string, number, null]> = zip(['a'], [1], [null])
 
-    expect([...it]).toMatchObject([['a', 1, null]])
+  it('types', () => {
+    const arr: [string, number, null][] = pipe(zip(['a'], [1], [null]), toArray)
+
+    expect(arr).toMatchObject([['a', 1, null]])
+  })
+  it('types', () => {
+    const it = iter(range(1, 12))
+
+    const zipped = pipe(zip(it, it, it), toArray)
+
+    expect(zipped).toMatchObject([
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ])
   })
 })
