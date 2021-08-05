e6777b85c4e5d79983649056ff2c81b1dffac10b
PatrykWalach <patryk_walach@outlook.com>
2021-08-05 17:45:46 +0200

    refactor(starmap)

--- tests/unit/itertools/starmap.spec.ts
+++ tests/unit/itertools/starmap.spec.ts
@@ -1,10 +1,14 @@
-import { zip } from '../../../src'
+import { pipe, toArray, zip } from '../../../src'
 import { starmap } from '../../../src/itertools/starmap'
 
 describe('starmap()', () => {
   it('w', () => {
-    expect([
-      ...starmap(zip([1, 2, 3], [3, 4, 5]), (a, b) => a + b),
-    ]).toMatchObject([4, 6, 8])
+    expect(
+      pipe(
+        zip([1, 2, 3], [3, 4, 5]),
+        starmap((a, b) => a + b),
+        toArray,
+      ),
+    ).toMatchObject([4, 6, 8])
   })
 })
